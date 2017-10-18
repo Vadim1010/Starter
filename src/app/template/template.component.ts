369aa8bcd3fe723e9497e1a5d61426d859461683
Vadim <vv342915007@gmail.ru>
2017-10-18 10:16:40 +0300

    create template

---
+++ src/app/template/template.component.ts
@@ -0,0 +1,14 @@
+import {
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'template',
+  templateUrl: 'template.component.html',
+  styleUrls: ['template.component.scss'],
+  encapsulation: ViewEncapsulation.Native
+})
+export class TemplateComponent {
+
+}
---
+++ src/app/template/template.module.ts
@@ -0,0 +1,15 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TemplateComponent } from './template.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+  ],
+  declarations: [
+    TemplateComponent
+  ],
+  exports: [TemplateComponent],
+})
+export class TemplateModule {
+}
