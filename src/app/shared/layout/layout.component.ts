2cb18c9e935ff83ecb405666f62dc323fbab1dd4
Dmytro Putrin <Dmytro_Putrin@epam.com>
2017-10-18 17:14:04 +0300

    TP: add layout component

---
+++ src/app/shared/layout/layout.component.ts
@@ -0,0 +1,14 @@
+import {
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'template',
+  templateUrl: 'layout.component.html',
+  styleUrls: ['layout.component.scss'],
+  encapsulation: ViewEncapsulation.Native
+})
+export class TemplateComponent {
+
+}
---
+++ src/app/shared/layout/layout.module.ts
@@ -0,0 +1,15 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TemplateComponent } from './layout.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+  ],
+  declarations: [
+    TemplateComponent
+  ],
+  exports: [TemplateComponent],
+})
+export class TemplateModule {
+}
