90e5f02bfe1c529074d31e7f55b31effabad1b03
Dmytro Putrin <Dmytro_Putrin@epam.com>
2017-10-20 17:51:26 +0300

    TT: move login warning text to config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,7 @@
-/**
- * Created by Dmytro_Putrin on 10/19/2017.
- */
+export const configObj = {
+  loginWarning: 'This system is owned by EPAM Systems. If you are not authorized to access this system, exit immediately. ' +
+  'Unauthorized access to this system is forbidden by company policies, national, and international laws. ' +
+  'Unauthorized users are subject to criminal and civil penalties as well as company initiated disciplinary ' +
+  'proceedings. By entering this system you acknowledge that you have authorized access and appropriate of ' +
+  'privilege within the system.'
+};
--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -2,7 +2,7 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
-
+import { configObj } from '../../app.config';
 @Component({
   selector: 'tt-login',
   templateUrl: 'login.component.html',
@@ -10,4 +10,5 @@ import {
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent{
+  public warningText: string = configObj.loginWarning;
 }
